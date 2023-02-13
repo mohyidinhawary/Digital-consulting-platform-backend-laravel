5453490c7b48538588c7d4255a7d3f81e1c47665
mohyidin <mohyidin2022@gmail.com>
2023-02-13 19:28:59 +0300

    my first project

---
+++ dcp-project/app/Http/Controllers/api/ExpertAvailabilityController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\api;
+use Carbon\Carbon;
+use App\Http\Controllers\Controller;
+use App\Models\ExpertAvailability;
+use Illuminate\Http\Request;
+class ExpertAvailabilityController extends Controller
+{
+    public function AddTimes(Request $request,$id){
+        //validation
+        $request->validate([  
+        "day"=>"required",
+        "date"=>"required",
+        "from"=>"required",
+        
+        ]);
+       
+        // create time+save
+        $available=new ExpertAvailability();
+        $available->expert_id=$id;
+        $e=ExpertAvailability::where('expert_id',$id)->where('day',$request->day)->where('date',$request->date)->where('from',$request->from)->first();
+        if($e)
+        {
+            return response()->json([
+                "message"=>"you added this time"
+            ]);
+        }
+        $available->day=$request->day;
+        $available->date=$request->date;
+        $available->from=$request->from;
+        $test=Carbon::createFromFormat('G:i:s',$available->from);
+        $test=$test->addHour();
+        $available->to=$test;
+        $available->save();
+        
+        
+        // send response
+        return response()->json([
+        'status'=>1,
+        'massege'=>'Times added successfully',
+        $available->to
+        ],200);
+        
+        }
+
+    }
---
+++ dcp-project/app/Http/Controllers/api/FavoriteController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Favorite;
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Expert;
+use Illuminate\Support\Facades\DB;
+class FavoriteController extends Controller
+{
+    public function AddExpert($id1,$id2){
+        $favorite=new Favorite();
+        if(DB::table('favorites')->where('user_id',$id1)->first()&&DB::table('favorites')->where('expert_id',$id2)->first()){
+            return response()->json([
+                "message"=>"already added"
+            ]);
+        }
+        $favorite->user_id=$id1;
+        $favorite->expert_id=$id2;
+        $favorite->save();
+        return response()->json([
+        'status'=>1,
+        'message'=>'expert added successfully'
+        ],200); 
+    } 
+    public function deleteExpert($id1,$id2){
+        $favorite=Favorite::where('expert_id',$id1)->where('user_id',$id2)->delete();
+        return response()->json([
+            'status'=>1,
+            'message'=>'expert deleted successfully'
+            ],200); 
+    } 
+    
+}
